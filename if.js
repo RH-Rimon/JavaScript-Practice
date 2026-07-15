3122054ee8b9fd3a1de575cb168eed7006a666ea
RH-Rimon <rrimon86@gmail.com>
2026-07-16 01:52:52 +0600

    some problem

--- if.js
+++ if.js
@@ -3,3 +3,8 @@
 if(true){
     console.log('inside the condition');
 }
+
+
+if(8>3){
+    console.log('Yes 8 is greater than 3');
+}
---
+++ multiple.js
@@ -0,0 +1,19 @@
+// Multiple condition
+
+const salary = 25000;
+const isBcs = true;
+const hasCar = true;
+
+if (salary>50000 && isBcs === true){
+    console.log('su..........patro');
+}
+
+else{
+    console.log('kupatro');
+}
+
+
+if(salary>75000 || isBcs === true || hasCar){
+    console.log('bolo baba kobul');
+}
+
